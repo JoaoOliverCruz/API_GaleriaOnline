2c8bba00a5db6ff537bd6dc034484e2ff58c7e68
Joao Pedro <joaocruz.oliver17@gmail.com>
2025-08-19 16:52:23 -0300

    feat: Atualizando a minha api, colocando cors e static files

--- GaleriaOnline.WebApi/obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
+++ GaleriaOnline.WebApi/obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac88f2b82a259d7ff03074c5d9a2352bcd383f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ GaleriaOnlineScript.sql
@@ -0,0 +1,6 @@
+SELECT TOP (1000) [Id]
+      ,[Nome]
+      ,[Caminho]
+  FROM [GaleriaOnlineDB].[dbo].[Imagem]
+
+
